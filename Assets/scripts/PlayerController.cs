d8cbe77309c58c382f67932691a9a12d77431428
whiterocket <aljaz.podgornik@t-2.net>
2019-02-15 19:11:26 +0100

    Created player

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Object bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector2(-0.5f , -7.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int movement = (int)Input.GetAxisRaw("Horizontal");
+
+
+        if (movement == 1 && this.transform.position.x < 13)
+        {
+            this.transform.position = new Vector2(this.transform.position.x + 0.2f, this.transform.position.y);
+        }
+
+        else if (movement == -1 && this.transform.position.x > -13)
+        {
+            this.transform.position = new Vector2(this.transform.position.x - 0.2f, this.transform.position.y);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(bullet, new Vector3(this.transform.position.x,-7,0), Quaternion.identity);
+        }
+    }
+}
