a77b0e30f91d6dd8694c3490ce69686bad9bd44c
whiterocket <aljaz.podgornik@t-2.net>
2019-02-18 23:19:01 +0100

    created a n enemy bullet

---
+++ Assets/scripts/BigBulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 0.25f);
+
+        if (this.transform.position.y < -10)
+        { 
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
